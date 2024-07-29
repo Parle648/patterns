28ee91c8669dd0e430a010d285354a2786c90cd4
Parle648 <sashahoumi@gmail.com>
2024-07-29 14:05:00 +0300

    - ListService at server folder

--- server/src/services/list.service.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from "../data/models/card";
-import { List } from "../data/models/list";
-
-class ListService {
-    
-}
-
-export { ListService };
--- server/src/services/services.ts
+++ server/src/services/services.ts
@@ -1,2 +1,1 @@
 export { ReorderService } from "./reorder.service";
-export { ListService } from './list.service';
